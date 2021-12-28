da8eb559e7b6dd0503ce7f1561aa295e50e065a6
Kotolis <94461589+Kotolis@users.noreply.github.com>
2021-12-28 19:23:01 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -7,4 +7,5 @@ link = 'https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 def test_guest_should_see_button_add_to_basket(browser):
     browser.get(link)
     time.sleep(5)
-    assert browser.find_element_by_css_selector("button.btn-add-to-basket").is_displayed() and browser.find_element_by_css_selector("button.btn-add-to-basket").is_enabled()
+    btn = len(browser.find_elements_by_css_selector("button.btn-add-to-basket"))
+    assert btn > 0, "Кнопка добавления отсутствует"
